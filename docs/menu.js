c2c9b1ecc5eed276e3b324757d0106f5bfe2e9b7
bmoren <ben@benmoren.com>
2019-11-14 22:13:28 -0600

    unify menu across languages

---
+++ docs/menu.js
@@ -0,0 +1,13 @@
+document.write(`
+
+<header id="page-header">
+  <ul id="language-selection">
+    <li><a href="index.html">English</a></li>
+    <li><a href="fr.html">Français</a></li>
+    <li><a href="de.html">Deutsche</a></li>
+    <li><a href="ja.html">日本語</a></li>
+    <li><a href="nl.html">Nederlands</a></li>
+  </ul>
+</header>
+
+`)
