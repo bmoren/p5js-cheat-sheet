3960aceba666ffa4c087333cdfa4ea555b2e5e82
bmoren <ben@benmoren.com>
2019-11-14 22:26:10 -0600

    add link to reference

--- docs/menu.js
+++ docs/menu.js
@@ -7,6 +7,9 @@ document.write(`
     <li><a href="de.html">Deutsche</a></li>
     <li><a href="ja.html">日本語</a></li>
     <li><a href="nl.html">Nederlands</a></li>
+    <li>|</li>
+    <li><a href="https://p5js.org/reference/">p5js Reference</a></li>
+
   </ul>
 </header>
 
